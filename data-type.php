88579da477f9a0b36e8be0fe40de8ce9ad5f86f9
arasruislam <arasru01@gmail.com>
2025-07-04 03:22:06 +0600

    data type

---
+++ data-type.php
@@ -0,0 +1,30 @@
+<?php
+// String
+$name = "ashraful";
+
+var_dump($name);
+
+// Integer
+$num = 19;
+var_dump($num);
+
+// float
+$floatNum = 12.2;
+var_dump($floatNum);
+
+// boolean
+$bool = true;
+var_dump($bool);
+
+// array
+$names = ["Ashraful", "Shehab", "kamal"];
+var_dump($names);
+
+// function
+class Course {};
+$course = new Course();
+var_dump($course);
+
+// Null
+$example = NULL;
+var_dump($example);
--- variable.php
+++ variable.php
@@ -3,3 +3,5 @@
 $name = 'ashraful';
 
 echo $name;
+
+?>
