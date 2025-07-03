3dbf708de583575a1bae7d69e56c4fac9c5c5886
arasruislam <arasru01@gmail.com>
2025-07-04 04:06:32 +0600

    ...

--- loop.php
+++ loop.php
@@ -1,8 +1,23 @@
 <?php
 
-for($x = 0; $x <= 10; $x++){
+for ($x = 0; $x <= 10; $x++) {
    echo $x;
    echo PHP_EOL;
 }
 
-?>
+$total = 0;
+echo PHP_EOL;
+
+for ($x = 0; $x <= 10; $x++) {
+   $total += $x;
+}
+
+echo $total;
+echo PHP_EOL;
+
+$arr = ['ashraful', "city university", "Dhaka, Bangladesh"];
+
+for ($x = 0; $x < count($arr); $x++) {
+   echo $arr[$x];
+   echo PHP_EOL;
+}
